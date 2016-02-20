d5307ab78ae1e1b981837b38665cccf100e0e612
Rajath Krishnan <krishnan@iastate.edu>
2016-02-19 23:54:42 -0600

    Added a test scene where we move a cube around using the arrow keys

---
+++ Drawculus/Assets/Wiimote/Plugins/mac/hidapi.bundle/Contents/MacOS/cube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+    
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+	   moveSpeed = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime, 0f, Input.GetAxis("Vertical")*Time.deltaTime); // x, y, z
+	}
+}
